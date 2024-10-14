d494ed3b4a069e53f893ff4dec170fad72b350f7
조연경 <sig.yg5057@gmail.com>
2024-10-14 19:01:26 +0900

    MainLayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 
 const MainWrapper = styled.section`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
